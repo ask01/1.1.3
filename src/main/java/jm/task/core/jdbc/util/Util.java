3ac5f32df72e48d9c219187b81447ec075ce3b68
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection getconnection() {
+        Connection connection = null;
+
+
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+        } catch (SQLException e) {
+            System.err.println("Ошибка подключения");
+        }
+        return connection;
+    }
 }
+
+
+
+
+
+
+
